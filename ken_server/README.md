ffa6ff2c9c5e9c590b04f1af5ed7adc30db81fac
Shaw Ken Chang <digitalkc408@gmail.com>
2014-12-01 19:21:57 -0800

    Uploaded Ken Server and Ken Client folders and files

---
+++ ken_server/README.md
@@ -0,0 +1,39 @@
+CMPE207 project - by Network Ninjas 
+============
+## Project scope
+* web/mobile applications for picture sharing
+* 4 client-server pairs
+* 4 servers communicate with each other to sync pictures (the picuture files themselves and the picutre info)
+
+## Contract & protocol to follow
+The guidelines below should be followed by everyone in order to have predictable hehavior in server-server communications. The methods for sending files/notifications are defined in file_server_lib.php; include it in your scripts whenever you need to call them.
+* a picture has 4 properties(for simplicity we call them the 'picture info'):
+  - id: derived from md5 value of the picture file. Required
+  - title: title of the picture. Required
+  - category: category of a picture, e.g scenery, sports, people etc. Optional
+  - desc: the description of a picture. Optional
+- the file and info can be stored on your server whatever way you want; but it is recommended to store the file on filesystem, then keep an record in the MySQL database which contains the file's path, timestamp and the picture info
+* a server send post request to its peers when:
+  - it receives new file from client. In this case it sends the new file together with picture info to other servers
+  - file info is updated somehow by it or its client. In this case it notifies and sends picture info to other servers
+  - file is deleted somehow by it or its client. In this case it notifies and sends the picture id to other server
+* servers can use different path to handle requests, but make sure you have updated your server's path information in file_server_lib.php's configuration block 
+* all servers use the same library methods to send files and send update/delete notifications 
+  - method for sending files: send_to_peers
+  - method for update notification: notify_update
+  - method for delete notification: notify_delete
+* see comments in file_server_lib.php  for details
+
+## Things everyone has to implement by self
+The following parts can vary from server to server and need to be implementated individually 
+* any communications that occurs between server and client
+* logic for receiving files from peers (upload_from_peer.php)
+* logic for receiving update/delete notifications from peers (notify_from_peer.php)
+* logic for storing the file on your server(in DB or on FS; directory and filename etc.)
+* logic for keeping track of picture info associated with the files(recommended to keep records in MySQL)
+
+## Directory structure
+The shared files(like file_server_lib.php is in the root directory); you need to create a folder for your part. Remember not to commit any confidentials(like password for your MySQL connection etc) to GitHub.
+
+## Helpful docs & resources
+* get md5 value of a file: [PHP document for md5_file](http://php.net/manual/en/function.md5-file.php)
---
+++ ken_server/file_server_lib.php
@@ -0,0 +1,158 @@
+<?php
+/**
+ * This library will be used across all php file servers to implement server-server communication
+ * Please read comments in the common configuration block and keep these convention(like file receive page name)
+ */
+
+// ======================================================================================================
+// Common configuration block begins
+// ======================================================================================================
+
+
+// Read 'SERVER_NAME' from server variable so when sending files it won't send to itself
+// To set 'SERVER_NAME' on your server, in all pages that require this library, add this line in the beginning:
+// putenv ("SERVER_NAME=$your server name$")
+// where $your server name$ is 'annie'|'daniel'|'ken'|'yulin' depend on who you are
+$server_name = $_SERVER['SERVER_NAME'];
+
+// TODO: edit your entry to reflect the real path to the scripts
+$peer_list = array(
+  // 'annie' =>  array(
+  //   'upload_path' => 'http://www.annie-suantak.com/???',
+  //   'notify_path' => 'http://http://www.annie-suantak.com/???'
+  //   ),
+  // 'daniel'=>  array(
+  //   'upload_path' => 'http://www.danielishere.com/???',
+  //   'notify_path' => 'http://http://www.danielishere.com/???'
+  //   ),
+  'ken'   =>  array(
+    'upload_path' => 'http://www.skctech.net/PhotoSharing/upload_from_peer.php',
+    'notify_path' => 'http://www.skctech.net/PhotoSharing/notify_from_peer.php'
+    ),
+  'yulin' =>  array(
+    'upload_path' => 'http://www.yulinye.com/fileserver/upload_from_peer.php',
+    'notify_path' => 'http://www.yulinye.com/fileserver/notify_from_peer.php'
+    )
+  );
+
+// ======================================================================================================
+// Protocol methods definition block begins
+// ======================================================================================================
+
+/**
+ * Send a picture file and its infomation to all peer servers so everyone is keep in sync
+ * Should be called after your server received upload from your client
+ * Implementation of the receiving script is required; everyone can handle file & DB operations differently 
+ *  
+ * @param  string $file_path The path of the file; it is recommended to use [md5_id].[ext] to store files
+ * @param  string $md5_id    The md5 of the file, used to distinguish files
+ * @param  string $title     Title of the picture, which is different from file name
+ * @param  string $category  A string that describes the category of the picture
+ * @param  string $desc      The description of the picutre
+ * @return boolean           A boolean indicate if the upload to all peer servers is successful(true for success)
+ */
+function send_to_peers($file_path, $md5_id, $title, $category, $desc){
+  global $peer_list;
+  global $server_name;
+
+  Util::log('Debug: send to peers called');
+  $success = TRUE;
+  foreach($peer_list as $peer_name => $path_arr) {
+    Util::log('Debug: start to send to the server ['.$peer_name.']');
+    Util::log('Debug: the path of the destination is ['.$path_arr['upload_path'].']');
+    if ($peer_name != $server_name) { // avoid send to self
+      // get full path of a file on server
+      Util::log('Debug: passed the test that it is not self');
+      $file_name_with_full_path = realpath($file_path);
+      // construct the array for curl post action
+      $post_arr = array('action'=>'peer_upload', 'file'=>'@'.$file_name_with_full_path, 'from'=>$server_name, 'md5_id'=>$md5_id, 'title'=>$title, 'category'=> $category, 'desc'=>$desc);
+      // curl operation
+      $result = curl_post($path_arr['upload_path'], $post_arr);
+      Util::log('Debug: result from the cur_post: '.$result);
+      // update status
+      $success = $success && $result;
+    }
+  }
+
+  return $success;
+}
+
+/**
+ * Notify peer server that the infomation of a file has been changed to keep all peers in sync
+ * Should be called after infomation of the file(other than the file itself) is modified
+ * Implementation of the receiving script is required; everyone can handle file & DB operations differently
+ * 
+ * @param  string $md5_id    The md5 of the file, used to distinguish files
+ * @param  string $title     Title of the picture, which is different from file name
+ * @param  string $category  A string that describes the category of the picture
+ * @param  string $desc      The description of the picutre
+ * @return boolean           A boolean indicate if the notification to all peer servers is successful(true for success)
+ */
+function notify_update($md5_id, $title, $category, $desc) {
+  global $peer_list;
+  global $server_name;
+
+  $success = TRUE;
+  foreach($peer_list as $peer_name => $path_arr) {
+    if ($peer_name != $server_name) { // avoid send to self
+      // construct the array for curl post action
+      $post_arr = array('action'=>'update', 'from'=>$server_name, 'md5_id'=>$md5_id, 'title'=>$title, 'category'=> $category, 'desc'=>$desc);
+      // curl operation
+      $result = curl_post($path_arr['notify_path'], $post_arr);
+      // update status
+      $success = $success && $result;
+    }
+  }
+
+  return $success;
+}
+
+/**
+ * Notify peer server that the a file has been deleted to keep all peers in sync
+ * Should be called after a file is deleted
+ * Implementation of the receiving script is required; everyone can handle file & DB operations differently
+ * @param  string $md5_id    The md5 of the file, used to distinguish files
+ * @return boolean           A boolean indicate if the notification to all peer servers is successful(true for success)
+ */
+function notify_delete($md5_id) {
+  global $peer_list;
+  global $server_name;
+  
+  $success = TRUE;
+  foreach($peer_list as $peer_name => $path_arr) {
+    if ($peer_name != $server_name) { // avoid send to self
+      // construct the array for curl post action
+      $post_arr = array('action'=>'delete', 'from'=>$server_name, 'md5_id'=>$md5_id);
+      // curl operation
+      $result = curl_post($path_arr['notify_path'], $post_arr);
+      // update status
+      $success = $success && $result;
+    }
+  }
+
+  return $success;
+}
+
+
+// ======================================================================================================
+// Helper functions definition block begins
+// ======================================================================================================
+
+/**
+ * The common method to send a curl post request
+ * @param  string   $url      Recipient url
+ * @param  array    $post_arr An array used in curl post
+ * @return boolean            A boolean indicate if the post action is successful(true for success)
+ */
+function curl_post($url, $post_arr){
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $post_arr);
+  $result=curl_exec ($ch);
+  curl_close ($ch);
+  Util::log('Debug: result from the curl_exec: '.$result);
+  return $result;
+}
+
+?>
