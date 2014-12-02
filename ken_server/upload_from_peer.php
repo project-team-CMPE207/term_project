5a74cdd822a9e59700b80710ac2f79d7053c35cf
Shaw Ken Chang <digitalkc408@gmail.com>
2014-12-01 19:27:18 -0800

    uploaded bunch of things in ken server

--- ken_server/upload_from_peer.php
+++ ken_server/upload_from_peer.php
@@ -26,30 +26,24 @@ define("MAXSIZE", 4096*1000);
 // ======================================================================================================
 // Main block begins
 // ======================================================================================================
-
-Util::log("Request received: client upload");
+Util::log("Request received: peer upload");
 
 // extract picture information
 $file     = $_FILES['file'];
 $from     = $_POST['from'];
+$md5_id   = $_POST['md5_id'];
 $title    = $_POST['title'];
 $category = $_POST['category'];
 $desc     = $_POST['desc'];
 
 // check file data
 if(!$file) {
-  Util::log_and_die("Bad client upload request: no file data");
+  Util::log_and_die("Bad peer upload request: no file data");
 }
 
 // check required field
-if(!$from || !$title) {
-  Util::log_and_die("Bad client upload request: required fields are missing");
-}
-
-// generate md5 id for uploaded file
-$md5_id = md5_file($file["tmp_name"]);
-if (!$md5_id) {
-  Util::log_and_die("Server error: can't generate md5 id for uploaded file");
+if(!$file || !$from || !$md5_id || !$title) {
+  Util::log_and_die("Bad peer upload request: required fields are missing");
 }
 
 // initiate db connection
@@ -57,17 +51,18 @@ FileDB::init();
 
 // duplication check
 if (FileDB::check_duplicate($md5_id)){
-  Util::log_and_die("Duplicated file for ".$md5_id);
+  Util::log_and_die("Bad peer upload request: duplicated file for ".$md5_id);
 };
 
 // type and size check
-$type = strtolower(pathinfo($file["name"],PATHINFO_EXTENSION));
+$type = strtolower(pathinfo($_FILES["file"]["name"],PATHINFO_EXTENSION));
 $size = $_FILES['file']['size'];
 if ($size>MAXSIZE) {
   Util::log_and_die("Bad client upload request: file exceed size limit(".MAXSIZE."kb)");
 } elseif (!in_array($type, $allowed_types)) {
   Util::log_and_die("Bad client upload request: unacceptable file format");
 }
+
 
 // build upload path
 $upload_dir   = "uploads/";
@@ -82,13 +77,6 @@ if ($success) {
 }
 
 FileDB::close();
+
 Util::log_and_echo("Request processed: file uploaded successfully");
-/*
-// send the new file to peer servers
-$success = send_to_peers($upload_path, $md5_id, $title, $category, $desc);
-if (!$success) {
-  Util::log("Response from peers: at least one peer didn't get the file");
-}
-Util::log("Response from peers: all peers received the file successfully!");
-*/
 ?>
